4b0dbf238e3bcada6cee79bc45200b168c7c80c5
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-03-26 17:08:13 +0000

    adds getSeats and addPassenger to plane

--- src/main/java/Plane.java
+++ src/main/java/Plane.java
@@ -1,13 +1,17 @@
+import java.util.ArrayList;
+
 public class Plane {
 
     private PlaneType planeType;
     private int capacity;
     private int weight;
+    private ArrayList<Passenger> passengers;
 
     public Plane(PlaneType planeType , int capacity, int weight) {
         this.planeType = planeType;
         this.capacity = capacity;
         this.weight = weight;
+        this.passengers = new ArrayList<Passenger>();
     }
 
     public PlaneType getType() {
@@ -20,5 +24,13 @@ public class Plane {
 
     public int getWeight() {
         return this.weight;
+    }
+
+    public int getSeats() {
+        return this.capacity - this.passengers.size();
+    }
+
+    public void addPassenger(Passenger passenger) {
+        this.passengers.add(passenger);
     }
 }
--- src/test/java/PlaneTest.java
+++ src/test/java/PlaneTest.java
@@ -6,10 +6,15 @@ import static org.junit.Assert.assertEquals;
 public class PlaneTest {
 
     Plane plane1;
+    Passenger passenger1;
+    Passenger passenger2;
 
     @Before
     public void before(){
-        plane1 = new Plane(PlaneType.GULFSTREAMG650, 5, 10000);
+        plane1 = new Plane(PlaneType.GULFSTREAMG650, 2, 10000);
+        passenger1 = new Passenger("Gareth", 2);
+        passenger2 = new Passenger("Aga", 3);
+
     }
 
     @Test
@@ -19,11 +24,22 @@ public class PlaneTest {
 
     @Test
     public void canGetCapacity(){
-        assertEquals(5, plane1.getCapacity());
+        assertEquals(2, plane1.getCapacity());
     }
 
     @Test
     public void canGetWeight(){
         assertEquals(10000, plane1.getWeight());
+    }
+
+    @Test
+    public void canGetAvailableSeats(){
+        assertEquals(2, plane1.getSeats());
+    }
+
+    @Test
+    public void canAddPassenger(){
+        plane1.addPassenger(passenger1);
+        assertEquals(1, plane1.getSeats());
     }
 }
